bf743eba05a017210586bf1cbc1a99a81ce953da
Bryon <bryon.pectol@gmail.com>
2021-12-13 12:06:47 -0700

    testing git

---
+++ js/wallpaper.js
@@ -0,0 +1,27 @@
+
+const wallpaperArray = ["/images/wallpaper/1 (","/images/wallpaper/1 ("]
+
+let container = document.getElementById("container");
+
+let count;
+
+count = 0;
+
+function Generate() {
+
+    let img = document.createElement("img"); 
+    let link = document.createElement("a");
+    
+    img.className = "artimg";
+
+    count++;
+
+    link.setAttribute("href", `/images/wallpaper/1 (${count}).jpg`);
+    img.setAttribute("src", `/images/wallpaper/1 (${count}).jpg`);
+
+    container.appendChild(link);
+    link.appendChild(img);
+    //console.log(count);
+}
+
+wallpaperArray.forEach(Generate);
