a72b41f85e0375e321d49e490f6355d838d1a85f
Bryon <bryon.pectol@gmail.com>
2021-12-14 08:39:40 -0700

    added explicit

---
+++ js/explicitwallpaper.js
@@ -0,0 +1,63 @@
+
+const imgs = ["/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (","/images/explicitwallpaper/1 (",];
+
+
+localStorage.getItem("age");
+
+let container = document.getElementById("container");
+
+let submit = document.getElementById("submit");
+let age = document.getElementById("age");
+let form = document.getElementById("form");
+
+//console.log(localStorage.getItem("age") + " " + localStorage.getItem("bool"));
+
+container.style.visibility = "hidden";
+
+if(localStorage.getItem("age") >= 18) {
+    container.style.visibility = "visible";
+
+    form.style.visibility = "hidden";
+    
+}
+else {
+    
+
+    alert("WARNING! This is an explicit webpage. You are required to be over 18 years of age to view!");
+    
+    container.style.visibility = "hidden";
+
+}
+
+function Submit() {
+
+    age = document.getElementById("age").value;
+
+
+    localStorage.setItem("age", age);
+
+    location.reload();
+}
+
+let count;
+
+count = 0;
+
+function Generate() {
+
+    let img = document.createElement("img"); 
+    let link = document.createElement("a");
+    
+    img.className = "wallimg";
+
+    count++;
+
+    link.setAttribute("href", `/images/explicitwallpaper/1 (${count}).jpg`);
+    img.setAttribute("src", `/images/explicitwallpaper/1 (${count}).jpg`);
+
+    container.appendChild(link);
+    link.appendChild(img);
+    //console.log(count);
+}
+
+imgs.forEach(Generate);
