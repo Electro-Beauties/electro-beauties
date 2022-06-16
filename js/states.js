2c2b185f7d5800c0d81913d20db7b28248136a99
Electro-Beauties <95945798+Electro-Beauties@users.noreply.github.com>
2022-06-16 11:58:45 -0600

    Update states.js

--- js/states.js
+++ js/states.js
@@ -73,7 +73,7 @@ function Generate() {
     link.setAttribute("href", `/images/States/${count}.jpg`);
     img.setAttribute("src", `/images/States/${count}.jpg`);
     link.setAttribute("target", "_blank");
-    pAlt.textContent = "State: " + states[count]: + " " + count;
+    pAlt.textContent = "State: " + states[count] + ": " + count;
 
     link.appendChild(pAlt);
     container.appendChild(link);
