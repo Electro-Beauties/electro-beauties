226c148728f18d9bd9beada50a9e92cdbc7f8fd7
Electro-Beauties <95945798+Electro-Beauties@users.noreply.github.com>
2022-07-14 00:42:06 -0600

    Update wallpapers.js

--- js/wallpapers.js
+++ js/wallpapers.js
@@ -1,1 +1,37 @@
 
+const imgs = ["","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 (","/images/wallpaper/1 ("];
+
+let container = document.getElementById("container");
+
+let pTotal = document.getElementById("imgtotal");
+
+pTotal.textContent = "Number of Images: " + imgs.length;
+
+container.className = "imgcon";
+
+let count;
+
+count = 0;
+
+function Generate() {
+
+    let img = document.createElement("img"); 
+    let link = document.createElement("a");
+    let pAlt = document.createElement("p");
+    
+    img.className = "wallimg";
+
+    count++;
+
+    link.setAttribute("href", `/images/wallpaper/1 (${count}).jpg`);
+    img.setAttribute("src", `/images/icons-wallpaper/1 (${count}).jpg`);
+    link.setAttribute("target", "_blank");
+    pAlt.textContent = "Image Number: " + count;
+
+    link.appendChild(pAlt);
+    container.appendChild(link);
+    link.appendChild(img);
+    //console.log(count);
+}
+
+imgs.forEach(Generate);
