0f82e6ece3a13e7ecbaa1df70cb48a37085878b1
Electro-Beauties <95945798+Electro-Beauties@users.noreply.github.com>
2022-07-14 17:35:01 -0600

    Create explicit wallpapers.js

---
+++ js/explicit wallpapers.js
@@ -0,0 +1,55 @@
+const desktopWallpapers = ["","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""];
+const mobileWallpapers = ["","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""];
+
+let container = document.getElementById("container");
+
+let pTotal = document.getElementById("imgtotal");
+
+pTotal.textContent = "Number of Images: " + desktopWallpapers.length;
+
+container.className = "imgcon";
+
+let count;
+
+count = 0;
+
+function Generate() {
+
+    let img = document.createElement("img"); 
+    let link = document.createElement("a");
+    let pAlt = document.createElement("p");
+    
+    img.className = "wallimg";
+
+    count++;
+
+    if (screen.width >= 450) {
+
+    link.setAttribute("href", `/images/explicit-desktop-wallpaper/${count}.jpg`);
+    img.setAttribute("src", `/images/explicit-desktop-wallpaper/${count}.jpg`);
+    link.setAttribute("target", "_blank");
+    pAlt.textContent = "Image Number: " + count;
+
+    }
+    else {
+    
+    link.setAttribute("href", `/images/explicit-mobile-wallpaper/${count}.jpg`);
+    img.setAttribute("src", `/images/explicit-mobile-wallpaper/${count}.jpg`);
+    link.setAttribute("target", "_blank");
+    pAlt.textContent = "Image Number: " + count;
+
+    }
+
+    link.appendChild(pAlt);
+    container.appendChild(link);
+    link.appendChild(img);
+    //console.log(count);
+}
+
+if(screen.width >= 450) {
+desktopWallpapers.forEach(Generate);
+}
+
+else {
+mobileWallpapers.forEach(Generate);
+}
